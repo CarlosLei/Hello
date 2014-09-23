256827634db0e4e716c495e5c7f9e282781f2d5b
CarlosLei <909208402@qq.com>
2014-09-23 22:29:08 +0800

    v1.2B
    实现了按钮关闭，信号与槽的连接

--- dialog.cpp
+++ dialog.cpp
@@ -7,12 +7,16 @@ Dialog::Dialog(QWidget *parent)
 {
     QPushButton *okBtn = new QPushButton(this);
     okBtn->setText(tr("OK"));
+    connect(okBtn,SIGNAL(clicked()),this,SLOT(close()));
+
     QPushButton *cancelBtn = new QPushButton(this);
-    cancelBtn->setText(tr("Cancel"));
+    cancelBtn->setText(tr("Cancel"));  
     cancelBtn->setGeometry(50,50,100,20);
+
     QPushButton *more = new QPushButton(this);
     more->setText(tr("More"));
     more->setGeometry(160,50,100,20);
+
     QLabel *label = new QLabel(this);
     label->setText(tr("Miss you!"));
     label->setGeometry(50,30,100,20);
