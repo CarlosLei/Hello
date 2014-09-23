40eec1e6143c7011eb2a2f0378c76590f44cc799
CarlosLei <909208402@qq.com>
2014-09-23 16:53:08 +0800

    V1.0 add okBtn

--- dialog.cpp
+++ dialog.cpp
@@ -1,8 +1,11 @@
 #include "dialog.h"
+#include <QPushButton>
 
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
 {
+    QPushButton *okBtn = new QPushButton(this);
+    okBtn->setText(tr("OK"));
 }
 
 Dialog::~Dialog()
