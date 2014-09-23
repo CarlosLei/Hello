805551fe5f6a4aefeb9b03ca0f13cd35340382a4
CarlosLei <909208402@qq.com>
2014-09-23 17:29:36 +0800

    v1.1 add two button

--- dialog.cpp
+++ dialog.cpp
@@ -6,6 +6,13 @@ Dialog::Dialog(QWidget *parent)
 {
     QPushButton *okBtn = new QPushButton(this);
     okBtn->setText(tr("OK"));
+    QPushButton *cancelBtn = new QPushButton(this);
+    cancelBtn->setText(tr("Cancel"));
+    cancelBtn->setGeometry(50,50,100,20);
+    QPushButton *more = new QPushButton(this);
+    more->setText(tr("More"));
+    more->setGeometry(160,50,100,20);
+
 }
 
 Dialog::~Dialog()
